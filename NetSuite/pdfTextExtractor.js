b50e210b18471cd93da3faa0c855513807d0a1de
Zach Karr <zacharykarr60@gmail.com>
2022-12-30 13:43:31 -0600

    Updates

--- NetSuite/pdfTextExtractor.js
+++ NetSuite/pdfTextExtractor.js
@@ -1,40 +1,40 @@
 const PDFExtract = require( 'pdf.js-extract' ).PDFExtract;
 const pdfExtract = new PDFExtract();
 const options = {};
-let pdfJSONData
-let sortedData = []
+let pdfJSONData;
+let sortedData = [];
 
-pdfExtract.extract( 'file.pdf', options, ( err, data ) =>
+pdfExtract.extract( "info.pdf", options, ( err, data ) =>
 {
     function getJSONData ()
     {
         if ( err ) return console.log( err );
-        pdfJSONData = data.pages
-        return pdfJSONData
+        pdfJSONData = data.pages;
+        return pdfJSONData;
     }
 
     class returnData
     {
         constructor ( data )
         {
-            this.data = data
+            this.data = data;
         }
         sortData ()
         {
 
             for ( var i = 0; i < pdfJSONData[ 0 ].content.length; i++ )
             {
-                this.data = pdfJSONData[ 0 ].content[ i ].str
-                sortedData.push( this.data )
+                this.data = pdfJSONData[ 0 ].content[ i ].str;
+                sortedData.push( this.data );
             }
-            return sortedData
+            return sortedData;
 
         }
     }
-    let returnedData = new returnData( getJSONData() ).sortData()
+    let returnedData = new returnData( getJSONData() ).sortData();
     for ( var i = 0; i < returnedData.length; i++ )
     {
-        console.log( returnedData[ i ] )
+        console.log( returnedData[ i ] );
     }
 
 } );
--- Puppeteer/messengerChatBot.js
+++ Puppeteer/messengerChatBot.js
@@ -1,6 +1,15 @@
-const { exec } = require( 'child_process' );
+
+const { Configuration, OpenAIApi } = require( "openai" );
+
 const puppeteer = require( 'puppeteer' );
+
 require( 'dotenv' ).config();
+
+const configuration = new Configuration( {
+
+    apiKey: process.env.OPENAI_API_KEY,
+
+} );
 
 ( async () =>
 {
@@ -22,17 +31,58 @@ require( 'dotenv' ).config();
 
     result = await page.evaluate( () =>
     {
+
         return Array.from( document.querySelectorAll( "div.x6prxxf.x1fc57z9.x1yc453h.x126k92a.xzsf02u" ) ).map( ( el ) => el.innerText );
+
     } );
+
 
     let oldMessage = result[ result.length - 1 ];
 
+    let img = '';
+    img = await page.evaluate( () =>
+    {
+        return Array.from( document.querySelectorAll( 'img.x1lliihq.x193iq5w.x5yr21d.xh8yej3' ) ).map( ( el ) => el.src );
+    } );
+
+    let oldimg = img[ img.length - 1 ];
+
+    console.log( oldimg );
+
+
     setInterval( async function ()
     {
+
         let date = new Date;
+
+        let img2 = await page.evaluate( () =>
+        {
+            return Array.from( document.querySelectorAll( 'img.x1lliihq.x193iq5w.x5yr21d.xh8yej3' ) ).map( ( el ) => el.src );
+        } );
+        let newimg = img2[ img2.legnth - 1 ];
+        console.log( newimg );
+
+        if ( oldimg != newimg )
+        {
+            const imgselector = '.xzsf02u.x1a2a7pz.x1n2onr6.x14wi4xw.x1iyjqo2.x1gh3ibb.xisnujt.xeuugli.x1odjw0f.notranslate';
+            page.type( imgselector, "Haha, that is a great picture!" ).then( () =>
+            {
+
+                page.keyboard.press( 'Enter' );
+
+            } );
+
+            oldimg = newimg;
+        } else if ( newimg == oldimg )
+        {
+            console.log( "Image hasnt changed" );
+        }
+
         let result2 = await page.evaluate( () =>
         {
+
             return Array.from( document.querySelectorAll( "div.x6prxxf.x1fc57z9.x1yc453h.x126k92a.xzsf02u" ) ).map( ( el ) => el.innerText );
+
         } );
 
         let newMessage = result2[ result2.length - 1 ];
@@ -42,38 +92,125 @@ require( 'dotenv' ).config();
 
         if ( oldMessage != newMessage )
         {
-            try
+
+            const selector = '.xzsf02u.x1a2a7pz.x1n2onr6.x14wi4xw.x1iyjqo2.x1gh3ibb.xisnujt.xeuugli.x1odjw0f.notranslate';
+
+
+            let formattedMessage = '';
+
+            if ( newMessage.length > 1000 )
             {
-                exec( process.env.CURL_CMD + process.env.CURL_CMD2 + newMessage + process.env.CURL_CMD3, ( err, stdout, stderr ) =>
+                let strLengthDivided = newMessage.length / 10;
+
+                let message = newMessage.slice( 0, strLengthDivided );
+                formattedMessage = message.split( /\r?\n\n/ );
+                console.log( formattedMessage );
+                console.log( formattedMessage.length );
+            } else
+            {
+                formattedMessage = newMessage.split( /\r?\n\n/ );
+            }
+
+            console.log( "formattedMessage", formattedMessage );
+
+            const openai = new OpenAIApi( configuration );
+
+            async function getText ()
+            {
+                try
                 {
-
-                    console.log( "err: ", err + '\n' );
-
-                    let json = JSON.parse( stdout );
-
-                    const selector = '.xzsf02u.x1a2a7pz.x1n2onr6.x14wi4xw.x1iyjqo2.x1gh3ibb.xisnujt.xeuugli.x1odjw0f.notranslate';
-
-                    console.log( "AI Response Was: " + json.choices[ 0 ].text + '\n' );
-
-                    page.type( selector, json.choices[ 0 ].text ).then( () =>
+                    let whosMessage = await page.evaluate( () =>
                     {
-                        page.keyboard.press( 'Enter' );
+                        let who = document.querySelectorAll( '.x1rg5ohu.x5yr21d.xl1xv1r.xh8yej3' );
+                        return who[ who.length - 1 ].alt.split( " " );
                     } );
 
-                } );
+                    console.log( 'whosMessage', whosMessage[ 0 ] );
 
-            } catch ( e )
-            {
-                console.log( e );
+                    if ( whosMessage[ 0 ] == "Person" )
+                    {
+                        console.log( "Ignore this person's messages" );
+                    } else
+                    {
+
+                        let checkIfAIisTyping = await page.evaluate( () =>
+                        {
+                            let textInput = document.querySelectorAll( '.xzsf02u.x1a2a7pz.x1n2onr6.x14wi4xw.x1iyjqo2.x1gh3ibb.xisnujt.xeuugli.x1odjw0f.notranslate' );
+                            return textInput[ 0 ].innerText;
+                        } );
+                        if ( checkIfAIisTyping != '\n' )
+                        {
+                            console.log( 'AI is currently typing' );
+                            newMessage = oldMessage;
+                        } else
+                        {
+                            console.log( 'AI is not currently typing' );
+
+
+                            const completion = await openai.createCompletion( {
+                                model: "text-curie-001",
+                                prompt: formattedMessage + " ",
+                                max_tokens: 1250,
+                                temperature: 0.6,
+                                presence_penalty: 0.5,
+
+                            } );
+                            let AIresponse = "Hey, " + whosMessage[ 0 ] + "! " + completion.data.choices[ 0 ].text.trim();
+                            console.log( "AI Response Was: " + AIresponse + '\n' );
+
+                            if ( completion.data.choices[ 0 ].text == "" )
+                            {
+
+                                page.type( selector, "It appears that the text you have provided is garbled and does not form coherent sentences. It is difficult for me to understand the intended meaning or topic of the text. Could you please provide a clear and concise question or statement for me to better assist you?" ).then( () =>
+                                {
+
+                                    page.keyboard.press( 'Enter' );
+
+                                } );
+                            }
+
+                            await page.type( selector, AIresponse ).then( () =>
+                            {
+
+                                page.keyboard.press( 'Enter' );
+
+
+                            } );
+                        }
+                    }
+
+                } catch ( error )
+                {
+
+
+                    if ( error.response )
+                    {
+
+                        console.log( error.response.status );
+
+                        console.log( error.response.data );
+
+                    } else
+                    {
+
+                        console.log( error.message );
+
+                    }
+                }
+
+                console.log( date );
+
+                console.log( 'A New Message Has Been Sent... Generating AI Response...' + '\n' );
+
+                oldMessage = newMessage;
             }
-            console.log( date );
-            console.log( 'A New Message Has Been Sent... Generating AI Response...' + '\n' );
-
-            oldMessage = newMessage;
+            getText();
 
         } else if ( newMessage == oldMessage )
         {
+
             console.log( date );
+
             console.log( 'The Message Has Not Changed... Waiting for New Message...' + '\n' );
 
         }

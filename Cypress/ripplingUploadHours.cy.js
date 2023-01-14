92d2b362dd2f6192768974227b107ac61699e1c1
Zach Karr <zacharykarr60@gmail.com>
2023-01-13 23:21:44 -0600

    Added ability to rename files with dates

--- Cypress/ripplingUploadHours.cy.js
+++ Cypress/ripplingUploadHours.cy.js
@@ -109,6 +109,7 @@ describe( 'IS KPI Automation Results:', () =>
         cy.wait( 5000 );
         cy.get( '[data-testid="Download"]' ).click();
         cy.get( '[data-testid="Download CSV"] > .css-1b5p2my-Content > span' ).click();
+        cy.exec( "cd cypress/downloads && mv 'Hours Per Day By Employee'.csv " + results + ".csv" + " " );
         cy.task( 'log', 'Downloading CSV...' );
         cy.task( 'log', '...Done!' + '\n' );
       }
@@ -127,16 +128,6 @@ describe( 'IS KPI Automation Results:', () =>
     cy.visit( 'https://integration.buildingstars.com/upload-hours' );
     cy.wait( 1000 );
     cy.task( 'log', '\n' + "Mark & Bob logged into Buildingstars' Integration Successfully! Beginning Upload..." + '\n' );
-
-    let fileList = [
-      'Hours Per Day By Employee.csv',
-      'Hours Per Day By Employee(1).csv',
-      'Hours Per Day By Employee(2).csv',
-      'Hours Per Day By Employee(3).csv',
-      'Hours Per Day By Employee(4).csv',
-      'Hours Per Day By Employee(5).csv',
-      'Hours Per Day By Employee(6).csv',
-    ];
 
     var getDaysArray = function ( s, e ) { for ( var a = [], d = new Date( s ); d <= new Date( e ); d.setDate( d.getDate() + 1 ) ) { a.push( new Date( d ) ); } return a; };
 
@@ -188,7 +179,7 @@ describe( 'IS KPI Automation Results:', () =>
 
       if ( weekends == 0 || weekends == 6 )
       {
-        fileList.splice( i, 0, 'Hours Per Day By Employee.csv' );
+
 
       } else
       {
@@ -198,15 +189,16 @@ describe( 'IS KPI Automation Results:', () =>
           years: dates[ 2 ]
         };
         let results = ripplingDate.years + "-" + ripplingDate.months + "-" + ripplingDate.days;
+        let fileList = ripplingDate.months + ripplingDate.days + ripplingDate.years;
         console.log( results );
 
         cy.get( '.space-y-1.text-center' )
           .find( '#csv' )
           .invoke( 'show' )
           .should( 'be.visible' )
-          .selectFile( Cypress.env( 'fileFolder' ) + fileList[ i ], { force: true } );
+          .selectFile( Cypress.env( 'fileFolder' ) + fileList + ".csv", { force: true } );
         cy.get( '#date' ).type( results );
-        cy.task( 'log', 'Uploading ' + fileList[ i ] + ' for date: ' + results );
+        cy.task( 'log', 'Uploading ' + fileList + ' for date: ' + results );
         cy.wait( 2000 );
         cy.get( '.ml-3' ).click();
         cy.task( 'log', 'Uploading CSV...' );
